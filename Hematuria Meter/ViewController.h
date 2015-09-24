644e67fd77fc037bb4161d6189a8a23943551491
Jim Ryan Quitevis <lordbritishix@Jims-MacBook-Pro.local>
2015-09-24 01:14:00 -0700

    initial commit

--- Hematuria Meter/ViewController.h
+++ Hematuria Meter/ViewController.h
@@ -8,8 +8,9 @@
 
 #import <UIKit/UIKit.h>
 
-@interface ViewController : UIViewController
-
-
+@interface ViewController : UIViewController <UIPageViewControllerDataSource>
+@property (strong, nonatomic) UIPageViewController *pageViewController;
+@property (strong, nonatomic) NSArray *pages;
+@property NSInteger currentPage;
 @end
 
--- Hematuria Meter/ViewController.m
+++ Hematuria Meter/ViewController.m
@@ -17,11 +17,70 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    _pages = @[@"Chart", @"Color1", @"Color2", @"Color3", @"Color4", @"Color5"];
+    
+    _currentPage = 0;
+    
+    self.pageViewController = [self.storyboard instantiateViewControllerWithIdentifier:@"PageViewController"];
+    self.pageViewController.dataSource = self;
+    
+    NSArray *viewControllers = @[[self viewControllerAtIndex:0]];
+    [self.pageViewController setViewControllers:viewControllers direction:UIPageViewControllerNavigationDirectionForward animated:NO completion:nil];
+    
+    [self addChildViewController:_pageViewController];
+    [self.view addSubview:_pageViewController.view];
+    [self.pageViewController didMoveToParentViewController:self];
 }
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
 }
+
+- (UIViewController *)pageViewController:(UIPageViewController *)pageViewController viewControllerBeforeViewController:(UIViewController *)viewController
+{
+    NSUInteger currentIndex = [_pages indexOfObject:viewController.restorationIdentifier];
+    _currentPage = currentIndex;
+    
+    if ((_currentPage - 1) < 0) {
+        return nil;
+    }
+    
+    _currentPage--;
+    
+    return [self viewControllerAtIndex:_currentPage];
+}
+
+- (UIViewController *)pageViewController:(UIPageViewController *)pageViewController viewControllerAfterViewController:(UIViewController *)viewController
+{
+    NSUInteger currentIndex = [_pages indexOfObject:viewController.restorationIdentifier];
+    _currentPage = currentIndex;
+    
+    if (_currentPage + 1 >= [_pages count]) {
+        return nil;
+    }
+    
+    _currentPage++;
+    
+    return [self viewControllerAtIndex:_currentPage];
+}
+
+- (UIViewController *)viewControllerAtIndex:(NSInteger)index
+{
+    UIViewController *controller = nil;
+    controller = [self.storyboard instantiateViewControllerWithIdentifier:[_pages objectAtIndex:index]];
+    return controller;
+}
+
+- (NSInteger)presentationCountForPageViewController:(UIPageViewController *)pageViewController
+{
+    return [self.pages count];
+}
+
+- (NSInteger)presentationIndexForPageViewController:(UIPageViewController *)pageViewController
+{
+    return 0;
+}
+
 
 @end
